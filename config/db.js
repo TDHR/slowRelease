7e6ff62f971e9a616daf2b6c1d4cf8fd3ce84dd3
TDHR <978481876@qq.com>
2018-10-02 09:07:31 +0800

    main ---国庆加班之启动定时任务

--- config/db.js
+++ config/db.js
@@ -1,11 +1,22 @@
 const mysql = require('mysql');
 const wrapper = require('co-mysql');
+// var dbConfig = {
+//     host:'59.110.171.208',
+//     user:'root',
+//     password:'nrc1qaz@WSX',
+//     database:'reits_data',
+//     port:3306,
+//     useConnectionPooling: true,
+//     charset:'utf8mb4'
+// };
 const pool = mysql.createPool({
-    host:'localhost',
-    port:'3306',
+    host:'59.110.171.208',
     user:'root',
-    password:'978481',
-    database:'koa2',
+    password:'nrc1qaz@WSX',
+    database:'reits_data',
+    port:3306,
+    useConnectionPooling: true,
+    charset:'utf8mb4'
 })
 const SqlP = wrapper(pool);
 
---
+++ config/scheduleProject.js
@@ -0,0 +1,55 @@
+// 定时任务，每15s生成一个二维码，并保存到数据库中
+const p = require('./db');
+const rpcMethod = require('../rpc/index');
+let  schedule = require('node-schedule');
+let addressList = ['1MDfA3XBZM5SMsuyyR23ZmGBQk9HCGmwV4','185Q5poNDgNzQkz3wzcRaSoNerf7naKBzY','1CUKmu8xg5AYetwLWvebkA1sQVoymfTZw7'];
+
+let rule = new schedule.RecurrenceRule();
+exports.createCode =async function() {
+
+    // var date = new Date(2018, 10, 1, 17, 45, 0);
+    rule.second = [0,30];
+
+
+    let j = schedule.scheduleJob(rule,function () {
+        initData();
+    });
+
+};
+//整理数据
+async function initData() {
+    let index = Math.floor((Math.random()*addressList.length));
+    let address = addressList[index];
+    let now =parseInt(new Date().getTime());
+    console.log(address);
+    let originMessage = `id:${now}`;
+    let singMessage  =await getSignMessage(address,originMessage);//获取签名信息；
+    // console .log(singMessage)
+    let  data = {address:address,originMessage:originMessage,signMessage:singMessage,codeId:now};
+
+    insertCode(data)
+}
+//将生成的二维码数据中插入数据库中
+async function insertCode(data) {
+    //s=HweOKK2CcM1vCDtAm6/Mg5hN2YzX+F1zCh3XnBein57xfoiF7UemhSlnzzWDr9w/pBaHrZBo1XzPS05NV/PJ948=&o=id:18419&id=18419&a=1BtvWKc9bdReVcP74WngsmuRiJrkjUiBNc
+
+    return new Promise((resolve,reject) => {
+        let sql = `insert into code_message (address,originMessage,signMessage,codeId) values('${data.address}','${data.originMessage}','${data.signMessage}',${data.codeId})`;
+        p.query(sql,function (error,results,fields) {
+            if(error) reject(error);
+            else {
+                console.log('插入成功');
+                resolve({
+                    message:'插入成功'
+                })
+            }
+        })
+    })
+}
+//获取签名信息
+async function getSignMessage(address,originMessage) {
+
+    let signMessage = await rpcMethod.signmessage(address,originMessage);
+    // console.log(signMessage)
+    return signMessage;
+}
